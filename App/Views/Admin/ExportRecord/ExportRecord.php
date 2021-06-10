8590b35461eabf6fa7030a139eae6e8410893fb3
Duc Cao <caovanducs@gmail.com>
2021-06-10 14:11:56 +0700

    Feature export records DB

--- App/Views/Admin/ExportRecord/ExportRecord.php
+++ App/Views/Admin/ExportRecord/ExportRecord.php
@@ -2,16 +2,16 @@
     <div class="col-sm-12">
         <h5>Export record</h5>
 
-        <button id="btnAnswer" class=" btn-call-api btn btn-primary">Answers</button>
-        <button id="btnLabels" class=" btn-call-api btn btn-primary">labels</button>
-        <button id="btnQuestioncategories" class=" btn-call-api btn btn-primary">questioncategories</button>
+        <button id="answers" class=" btn-call-api btn btn-primary">Answers</button>
+        <button id="labels" class=" btn-call-api btn btn-primary">labels</button>
+        <button id="questioncategories" class=" btn-call-api btn btn-primary">questioncategories</button>
 
-        <button id="btnQuestionqueue" class=" btn-call-api btn btn-primary">questionqueue</button>
-        <button id="btnQuetionqueue_labels" class=" btn-call-api btn btn-primary">quetionqueue_labels</button>
-        <button id="btnRatingsAnswer" class=" btn-call-api btn btn-primary">ratingsAnswer</button>
+        <button id="questionQueue" class=" btn-call-api btn btn-primary">questionqueue</button>
+        <button id="questionQueueLabel" class=" btn-call-api btn btn-primary">quetionqueue_labels</button>
+        <button id="rating-answers" class=" btn-call-api btn btn-primary">ratingsAnswer</button>
 
-        <button id="btnRatingsquestion" class=" btn-call-api btn btn-primary">ratingsquestion</button>
-        <button id="btnUsers" class=" btn-call-api btn btn-primary">users</button>
+        <button id="rating-questions" class=" btn-call-api btn btn-primary">ratingsquestion</button>
+        <button id="users" class=" btn-call-api btn btn-primary">users</button>
 
     </div>
 
@@ -50,37 +50,46 @@ const
     btnCopy.addEventListener("click", e => {
         const content = txtRecords.select();
 
-        document.execCommand("copy", false, content);
-        alert(content.val())
+        document.execCommand("copy", true, content);
+        txtRecords.text("");
+        alert("Đã copy");
         return;
     })
 
 
 
-
-    const btnAnswer = $("#btnAnswer");
-    const btnLabels = $("#btnLabels");
-    const btnQuestioncategories = $("#btnQuestioncategories");
-    const btnQuestionqueue = $("#btnQuestionqueue");
-    const btnQuetionqueue_labels = $("#btnqQuetionqueue_labels");
-    const btnRatingsAnswer = $("#btnRatingsAnswer");
-    const btnRatingsquestion = $("#btnRatingsquestion");
-    const btnUsers = $("#btnUsers");
-
-
-
-
-
     const btnCallAPIS = $(".btn-call-api");
 
+
+
     for (let i = 0; i < btnCallAPIS.length; ++i) {
-        console.log(btnCallAPIS[i]);
+
+        btnCallAPIS[i].addEventListener("click", e => {
+            console.log(btnCallAPIS[i]);
+
+            $.ajax({
+                url: `http://localhost:3000/api/${btnCallAPIS[i].id}`,
+                method: "get",
+                success: ret => {
+
+                    const data = JSON.stringify(ret.data);
+
+
+                    txtRecords.text("");
+                    txtRecords.text(data);
+                },
+                error: er => {
+
+                }
+
+            })
+
+        })
 
         btnCallAPIS[i].onclick = e => {
 
-            txtRecords.text("");
-            txtRecords.text(i);
-            alert(i);
+
+
 
         }
 
